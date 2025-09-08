26396f21d52cac03e6436b9cb26a1943dc12d972
Philipp Trenz <mail@philipptrenz.de>
2025-09-08 10:44:54 +0200

    improve install script

--- download_and_install.sh
+++ download_and_install.sh
@@ -17,4 +17,14 @@ echo "Build OpenV2Gx, this might take a while ..."
 make
 chmod +x /data/OpenV2Gx/Release/OpenV2G.exe
 
+echo "Download PyPLC for Venus OS"
+wget https://github.com/philipptrenz/pyPLC/archive/refs/heads/venus.zip -O tmp.zip
+unzip tmp.zip && rm tmp.zip
+[[ -d /data/pyPLC ]] && rm -r /data/pyPLC
+mv  pyPLC-venus/ /data/pyPLC
+cd  /data/pyPLC
+
+echo "Install pyPLC"
+source ./installer.sh
+
 echo "Done"
